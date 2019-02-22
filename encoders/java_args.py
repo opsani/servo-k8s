069ebdfea9ffb49556526e959816f89230eb4725
Johnius <me@johnius.ru>
2019-02-21 18:33:25 -0800

    java_args encoder refactoring

--- encoders/java_args.py
+++ encoders/java_args.py
@@ -1,143 +1,191 @@
 import yaml
 
 
-class JavaArgsEncoderException(Exception):
+# JavaOptsEncoderException = type('JavaOptsEncoderException', (BaseException,), {})
+# NoValueToEncodeException = type('NoValueToEncodeException', (JavaOptsEncoderException,), {})
+# NoValueToDecodeException = type('NoValueToDecodeException', (JavaOptsEncoderException,), {})
+# InvalidValueException = type('InvalidValueException', (JavaOptsEncoderException,), {})
+# NoLowerBoundException = type('NoLowerBoundException', (JavaOptsEncoderException,), {})
+# NoUpperBoundException = type('NoUpperBoundException', (JavaOptsEncoderException,), {})
+# UpperBoundViolationException = type('UpperBoundViolationException', (JavaOptsEncoderException,), {})
+# LowerBoundViolationException = type('LowerBoundViolationException', (JavaOptsEncoderException,), {})
+# BoundariesCollisionException = type('BoundariesCollisionException', (JavaOptsEncoderException,), {})
+# NoStepException = type('NoStepException', (JavaOptsEncoderException,), {})
+# NoTypeException = type('NoTypeException', (JavaOptsEncoderException,), {})
+# StepIsNotZeroException = type('StepIsNotZeroException', (JavaOptsEncoderException,), {})
+# ValueStepRemainderException = type('ValueStepRemainderException', (JavaOptsEncoderException,), {})
+# DuplicateSettingsException = type('DuplicateSettingsException', (JavaOptsEncoderException,), {})
+
+
+class MaxHeapSizeSetting:
+    name = 'MaxHeapSize'
+    type = 'range'
+    unit = 'gigabytes'
+    can_relax = True
+
+    def __init__(self, config):
+        self.check_config(config)
+        self.min = config.get('min')
+        self.max = config.get('max')
+        self.step = config.get('step')
+        self.default = config.get('default')
+
+    def check_config(self, config):
+        if not config:
+            raise Exception('No configuration provided for setting encoder {} in java-opts encoder.'.format(self.name))
+        minv = config.get('min')
+        maxv = config.get('max')
+        step = config.get('step')
+        if minv is None:
+            raise Exception('No min value provided for setting {} in java-opts encoder.'.format(self.name))
+        if maxv is None:
+            raise Exception('No max value provided for setting {} in java-opts encoder.'.format(self.name))
+        if step is None:
+            raise Exception('No step value provided for setting {} in java-opts encoder.'.format(self.name))
+        if not isinstance(minv, (int, float)):
+            raise Exception('Min value is neither int nor float in setting {} of java-opts encoder. '
+                            'It\'s value is "{}".'.format(self.name, minv))
+        if not isinstance(maxv, (int, float)):
+            raise Exception('Max value is neither int nor float in setting {} of java-opts encoder. '
+                            'It\'s value is "{}".'.format(self.name, maxv))
+        if not isinstance(step, (int, float)):
+            raise Exception('Step value is neither int nor float in setting {} of java-opts encoder. '
+                            'It\'s value is "{}".'.format(self.name, step))
+        if minv > maxv:
+            raise Exception('Lower boundary is higher than upper boundary in setting {} '
+                            'of java-opts encoder.'.format(self.name))
+        if minv == maxv and step > 0:
+            raise Exception('Step for setting must be 0 when min value == max value.')
+        # Relaxation of boundaries
+        if not self.can_relax:
+            default_min = getattr(self, 'min', None)
+            default_max = getattr(self, 'max', None)
+            if default_min is None:
+                raise Exception('Default min value for setting {} must be provided to disallow its relaxation.')
+            elif minv < default_min:
+                raise Exception('Min value for setting {} cannot be lower than {}. It is {} now.'.format(
+                    self.name, default_min, minv))
+            if default_max is None:
+                raise Exception('Default max value for setting {} must be provided to disallow its relaxation.')
+            elif maxv < default_max:
+                raise Exception('Max value for setting {} cannot be lower than {}. It is {} now.'.format(
+                    self.name, default_max, maxv))
+
+    def describe(self, data):
+        return (self.name, {
+            'type': self.type,
+            'min': self.min,
+            'max': self.max,
+            'step': self.step,
+            'value': self.decode_multi(data),
+            'unit': self.unit,
+        })
+
+    def validate_values(self, values):
+        value = values.get(self.name)
+        if value is None:
+            raise Exception('No value provided to encode for setting {}'.format(self.name))
+        if not isinstance(value, (float, int)):
+            raise Exception('Value in setting {} must be either integer or float, '
+                            'but we got "{}".'.format(value, self.name))
+        if value < self.min:
+            raise Exception('Value "{}" is violating lower bound in setting {}'.format(value, self.name))
+        if value > self.max:
+            raise Exception('Value "{}" is violating upper bound in setting {}'.format(value, self.name))
+        if self.min < self.max and (value - self.min) % self.step != 0:
+            raise Exception('Value "{}" is violating step requirement in setting {}'.format(value, self.name))
+        return value
+
+    def encode_multi(self, values):
+        value = self.validate_values(values)
+        return ['-XX:{}={}m'.format(self.name, int(round(value * 1024)))]
+
+    def validate_data(self, data):
+        found_args = list(filter(lambda arg: arg.startswith('-XX:{}'.format(self.name)), data))
+        if len(found_args) > 1:
+            raise Exception('Received duplicate values for setting {} on decode'.format(self.name))
+        if not found_args and self.default is None:
+            raise Exception('No value found to decode for setting {} and neither '
+                            'default value was provided.'.format(self.name))
+        return found_args
+
+    def decode_multi(self, data):
+        args = self.validate_data(data)
+        if args:
+            arg = args[0]
+            try:
+                return int(arg.split('=')[1].rstrip('m')) / 1024
+            except ValueError as e:
+                raise Exception('Invalid value to decode for setting {}. '
+                                'Error: {}. Arg: {}'.format(self.name, str(e), arg))
+        return self.default
+
+
+class GCTimeRatioSetting:
     pass
 
 
-EXC_RANGE_NO_VALUE = 'No value was provided for setting "{setting}" on encode in java_args encoder.'
-EXC_RANGE_NO_LOWER_BOUND = 'Lower bound for setting "{setting}" was not provided on encode in java_args encoder.'
-EXC_RANGE_LOWER_BOUND_VIOLATION = 'Value of setting "{setting}" is lower than defined lower bound ({left} < {right})' \
-                                  ' on encode in java_args encoder.'
-EXC_RANGE_NO_UPPER_BOUND = 'Upper bound for setting "{setting}" was not provided on encode in java_args encoder.'
-EXC_RANGE_UPPER_BOUND_VIOLATION = 'Value of setting "{setting}" is higher than defined upper bound ({left} < {right})' \
-                                  ' on encode in java_args encoder.'
-EXC_RANGE_BOUNDARIES_MISMATCH = 'Upper bound for setting "{setting}" is lower than the lower bound ' \
-                                '({left} < {right}) on encode in java_args encoder.'
-EXC_RANGE_NO_STEP = 'Step for setting "{setting}" was not provided on encode in java_args encoder.'
-EXC_RANGE_STEP_ZERO = 'Step for setting "{setting}" cannot be set to 0 on encode in java_args encoder.'
-EXC_RANGE_VALUE_STEP_REMAINDER = 'Value of setting "{setting}" cannot be divided by step without remainder' \
-                                 ' on encode in java_args encoder.'
-
-EXC_MULTI_ARGS = 'More than one argument for prefix "{prefixes}" is not supported on decode in java_args encoder.'
-
-EXC_UNPARSEABLE_VALUE = 'Could not parse value "{value}" for setting "{setting}" on decode in encoder java_args.\n' \
-                        'Error: {error}.'
-
-EXC_NO_CURRENT_VALUE = 'Could not find current value for setting "{setting}" on describe in encoder java_args. ' \
-                       'No default value has been provided.'
+supported_settings = {
+    'MaxHeapSize': MaxHeapSizeSetting,
+    # 'GCTimeRatio': GCTimeRatioSetting,
+}
 
 
-# Return list of zero or one arguments found in a list by specified prefix.
-def get_arg_from_list(arg_prefixes, args):
-    found_args = list(filter(lambda arg: any([arg.startswith(prefix) for prefix in arg_prefixes]), args))
-    if len(found_args) > 1:
-        raise JavaArgsEncoderException(EXC_MULTI_ARGS.format(prefixes='", "'.join(arg_prefixes)))
-    return found_args[0] if found_args else None
-
-
-def check_range(name, value, options):
-    assert value is not None, EXC_RANGE_NO_VALUE.format(setting=name)
-    minv = options.get('min', None)
-    maxv = options.get('max', None)
-    assert minv is not None, EXC_RANGE_NO_LOWER_BOUND.format(setting=name)
-    assert maxv is not None, EXC_RANGE_NO_UPPER_BOUND.format(setting=name)
-    assert minv <= maxv, EXC_RANGE_BOUNDARIES_MISMATCH.format(setting=name, left=minv, right=maxv)
-    assert value >= minv, EXC_RANGE_LOWER_BOUND_VIOLATION.format(setting=name, left=value, right=minv)
-    assert value <= maxv, EXC_RANGE_UPPER_BOUND_VIOLATION.format(setting=name, left=value, right=maxv)
-    step = options.get('step', None)
-    assert step is not None, EXC_RANGE_NO_STEP.format(setting=name)
-    assert step > 0, EXC_RANGE_STEP_ZERO.format(setting=name)
-    assert value % step == 0, EXC_RANGE_VALUE_STEP_REMAINDER.format(setting=name)
-
-
-class Encoder:
-
+class NewEncoder:
     def __init__(self, config):
-        assert config, 'No configratuion for encoder java_args has been provided.'
-        assert isinstance(config, dict), 'Configuration argument for encoder java_args expected to be a dictionary.'
-        assert 'settings' in config and config['settings'], 'No settings for encoder java_args has been provided.'
+        if config is None:
+            config = {}
+        assert isinstance(config, dict), 'Configuration object for java-opts encoder is expected to be ' \
+                                         'of a dictionary type'
         self.config = config
+        self.settings = {}
 
-    def describe(self):
-        settings = self.config.setdefault('settings', {})
-        described = {}
-        for name, opts in settings.items():
-            described[name] = opts.copy()
-        return described or None
+        requested_settings = self.config.get('settings') or {}
+        for name, setting in requested_settings.items():
+            try:
+                setting_class = supported_settings[name]
+            except KeyError:
+                raise Exception('Setting "{}" is not supported in jvm-opts encoder.'.format(name))
+            self.settings[name] = setting_class(setting or {})
+
+    def describe(self, data):
+        for setting in self.settings.values():
+            yield setting.describe(data)
 
     def encode_multi(self, values):
-        assert values, 'No values to encode provided to java_args encoder.'
-        before = self.config.get('before', [])
-        after = self.config.get('after', [])
-        settings = self.describe()
-        medium = []
-        for name, options in settings.items():
-            value = values.get(name, None)
-            if name == 'MaxHeapSize':
-                check_range(name, value, options)
-                medium.append('-XX:MaxHeapSize={}m'.format(int(value * 1024)))
-            if name == 'GCTimeRatio':
-                check_range(name, value, options)
-                medium.append('-XX:GCTimeRatio={}'.format(value))
-        return before + medium + after
+        values_to_encode = values.copy()
 
-    def decode_multi(self, data):
-        decoded = {}
-        settings = self.describe()
-        for name, options in settings.items():
-            if name == 'MaxHeapSize':
-                arg = get_arg_from_list(['-XX:MaxHeapSize'], data)
-                if arg:
-                    try:
-                        decoded[name] = int(arg.split('=')[1].rstrip('m')) / 1024
-                    except ValueError as e:
-                        raise JavaArgsEncoderException(EXC_UNPARSEABLE_VALUE.format(value=arg.split('=')[1].rstrip('m'),
-                                                                                    setting=name, error=str(e)))
-            if name == 'GCTimeRatio':
-                arg = get_arg_from_list(['-XX:GCTimeRatio'], data)
-                if arg:
-                    try:
-                        decoded[name] = int(arg.split('=')[1])
-                    except ValueError as e:
-                        raise JavaArgsEncoderException(EXC_UNPARSEABLE_VALUE.format(value=arg.split('=')[1].rstrip('m'),
-                                                                                    setting=name, error=str(e)))
-            # Handle default value
-            current_value = decoded.get(name, None)
-            default_value = options.get('default', None)
-            if default_value is not None and current_value is None:
-                decoded[name] = options['default']
-        return decoded
+        yield from self.config.get('before', [])
+
+        for name, setting in self.settings.items():
+            yield from setting.encode_multi({name: values_to_encode.pop(name, None)})
+
+        yield from self.config.get('after', [])
+
+        if values_to_encode:
+            raise Exception('We received settings to encode we do not support {}'.format(list(values_to_encode.keys())))
 
 
-def encode(encoder_options, values):
-    encoder = Encoder(encoder_options)
-    encoded = encoder.encode_multi(values)
-    assert encoded, 'No settings to encode on encode in java_args encoder. Got "{}" as input values.'.format(values)
-    return encoded
+def encode(config, values):
+    return list(NewEncoder(config).encode_multi(values))
 
 
-def describe(encoder_options, current_data):
-    encoder = Encoder(encoder_options)
-    desc = encoder.describe()
-    values = encoder.decode_multi(current_data)
-
-    for name, setting in desc.items():
-        try:
-            setting['value'] = values[name]
-        except KeyError as e:
-            raise JavaArgsEncoderException(EXC_NO_CURRENT_VALUE.format(setting=str(e).strip("'")))
-
-    return desc
+def describe(config, data):
+    return dict(NewEncoder(config).describe(data))
 
 
 if __name__ == '__main__':
     encoder_section = yaml.load(open('../config.yaml'))['k8s']['deployments']['d1']['command']['encoder']
-    input_data = ['java', '-server',
-                  '-XX:MaxHeapSize=2304m', '-XX:GCTimeRatio=92',
-                  '-javaagent:/tmp/newrelic/newrelic.jar', '-Dnewrelic.environment=l1', '-jar', '/app.jar']
-    data_to_encode = {'MaxHeapSize': 2.75, 'GCTimeRatio': 10}
-    # res = describe(options, input_data)
-    # print(res)
-    res = encode(encoder_section, data_to_encode)
-    print(res)
+
+    # Describe
+    current_data = ['java', '-server',
+                    '-XX:MaxHeapSize=1024m',
+                    '-XX:GCTimeRatio=92',
+                    '-javaagent:/tmp/newrelic/newrelic.jar', '-Dnewrelic.environment=l1', '-jar', '/app.jar']
+    desc = describe(encoder_section, current_data)
+    print('Described', desc)
+
+    # Encode
+    data_to_encode = {'MaxHeapSize': 13}  # , 'GCTimeRatio': 10}
+    encoded = encode(encoder_section, data_to_encode)
+    print('Encoded', encoded)
