1c188582776eb8f7ef2b43a466e4fbf93ace94ea
Fred Linkous Sharp <linkous8@gmail.com>
2020-05-22 12:51:56 -0500

    Added test for rollout ignores deployment ready

--- tst/test_adjust.py
+++ tst/test_adjust.py
@@ -1,5 +1,5 @@
 #
-from helpers import adjust, setcfg
+from helpers import adjust, setcfg, setup_deployment, cleanup_deployment, adjust_dep
 
 rep2 = {
   "state": {
@@ -54,3 +54,76 @@ def test_adjust(simple_app):
     assert mon["version_id"] == mon2["version_id"]
     assert mon["ref_spec_id"] == mon2["ref_spec_id"]
     assert mon["ref_version_id"] == mon2["ref_version_id"]
+
+def test_adjust_never_ready():
+    """
+    The following deployment will never be ready once mem is adjusted to 0.125Gi
+    the test verifies adjust ok is not reported in this case
+    """
+    dep = """
+    apiVersion: apps/v1
+    kind: Deployment
+    metadata:
+      name: test-adjust-never-ready
+    spec:
+      selector:
+        matchLabels:
+          app: test-adjust-never-ready
+      strategy:
+        rollingUpdate:
+          maxSurge: 25%
+          maxUnavailable: 25%
+        type: RollingUpdate
+      template:
+        metadata:
+          labels:
+            app: test-adjust-never-ready
+        spec:
+          containers:
+            - name: main
+              image: opsani/co-http
+              command:
+              - bash
+              - -c
+              - "if [ $(cat /sys/fs/cgroup/memory/memory.limit_in_bytes) -gt 191058816 ]; then /usr/local/bin/http; else sleep 1d; fi"
+              resources:
+                requests:
+                  cpu: "0.2"
+                  memory: "256Mi"
+                limits:
+                  cpu: "0.2"
+                  memory: "256Mi"
+              readinessProbe:
+                failureThreshold: 1000000
+                httpGet:
+                  path: /
+                  port: 8080
+                  scheme: HTTP
+                initialDelaySeconds: 30
+                periodSeconds: 10
+                successThreshold: 1
+                timeoutSeconds: 5
+    """
+    cfg = """
+    k8s:
+      application:
+        components:
+          test-adjust-never-ready:
+            settings:
+              mem:
+                min: .125
+                max: .5
+                step: .125
+    """
+    setup_deployment(dep)
+    captured_error = None
+    try:
+      adjust_dep(cfg, {'control': { 'timeout': 90 }, 'application': {'components': {'test-adjust-never-ready': {'settings': {'mem': {'value': .125}}}}}})
+    except Exception as e:
+      captured_error = e
+
+    assert captured_error is not None, 'Adjustment succeeded despite latest revision pods never becoming ready'
+    assert 'timed out waiting for replicas to come up' in str(captured_error), 'Adjustment error occurred but did not match the expected error. Error that occured: {}'.format(captured_error)
+    assert 'Rollback succeeded' in str(captured_error), 'The expected error occured but rollback failed. Error: {}'.format(captured_error)
+    
+    cleanup_deployment(dep)
